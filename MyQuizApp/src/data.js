9e2571aa54de82995b48931c2d2daef6d8dda19d
Sneha-Esakky <snehaesakky78@gmail.com>
2025-05-02 18:29:44 +0530

    Initial Commit

---
+++ MyQuizApp/src/data.js
@@ -0,0 +1,72 @@
+export const data = [
+    {
+        Question:'What is Redux ?',
+        Option1: 'It is State Management',
+        Option2: 'It is State',
+        Option3: 'It is part of HTML',
+        Option4: 'Redux is DataType'
+    },
+    {
+        Question: 'What is the primary use of Redux?',
+        Option1: 'To manage and centralize application state',
+        Option2: 'To create CSS styles',
+        Option3: 'To handle API requests',
+        Option4: 'To manage HTML templates'
+    },
+    {
+        Question: 'What are the core principles of Redux?',
+        Option1: 'State is distributed, changes are automatic',
+        Option2: 'State is shared, changes are made with impure functions',
+        Option3: 'Single source of truth, state is read-only, changes are made with pure functions',
+        Option4: 'None of the above'
+    },
+    {
+        Question: 'Which method is used to send data to the Redux store?',
+        Option1: 'send()',
+        Option2: 'update()',
+        Option3: 'setState()',
+        Option4: 'dispatch()'
+    },
+    {
+        Question: 'What is an action in Redux?',
+        Option1: 'A function to update the DOM',
+        Option2: 'A method to fetch API data',
+        Option3: 'A component method',
+        Option4: 'A plain JavaScript object describing what happened'
+    },
+    {
+        Question: 'What is a reducer in Redux?',
+        Option1: 'A pure function that specifies how the state changes in response to an action',
+        Option2: 'A function to fetch data',
+        Option3: 'A method to combine components',
+        Option4: 'A way to handle user input'
+    },
+    {
+        Question: 'What does the Redux store hold?',
+        Option1: 'All the components of the application',
+        Option2: 'The entire state tree of the application',
+        Option3: 'All the reducers',
+        Option4: 'All the actions'
+    },
+    {
+        Question: 'Which library is commonly used with Redux for React integration?',
+        Option1: 'Redux-React',
+        Option2: 'React-Store',
+        Option3: 'React-Redux',
+        Option4: 'Redux-Connect'
+    },
+    {
+        Question: 'What is middleware in Redux?',
+        Option1: 'A tool to update the state directly',
+        Option2: 'A function that intercepts actions before they reach the reducer',
+        Option3: 'A way to create actions',
+        Option4: 'A method to define reducers'
+    },
+    {
+        Question: 'What is the purpose of the combineReducers function?',
+        Option1: 'To create a new Redux store',
+        Option2: 'To dispatch multiple actions simultaneously',
+        Option3: 'To combine multiple reducers into a single reducer function',
+        Option4: 'To merge multiple states into one'
+    }    
+];
